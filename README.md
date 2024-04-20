042d5b252f3a2e0a6d62fb80cbe696ab43bfdc06
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-04-20 13:16:33 +0800

    Add proxy to prevent rate limit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # NGL Spammer
 Simple ngl.link spammer bot using python.
+
+# Installation
+
+```python
+pip install requests
+```
+
+```python
+python nglspammer.py
+```
--- nglspammer.py
+++ nglspammer.py
@@ -1,12 +1,18 @@
 import requests
-import time
+import random
+from itertools import cycle
 
-def send_post_spam(username, message):
+def send_post_spam(username, message, proxy):
     url = 'https://ngl.link/api/submit'
     payload = {'username': username, 'question': message, 'deviceId': ""}
-    headers = {'Content-Type': 'application/json'} 
-    response = requests.post(url, json=payload, headers=headers) 
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=payload, headers=headers, proxies=proxy)
     return response.status_code
+
+def read_proxies_from_file(filename):
+    with open(filename, 'r') as file:
+        proxies = [{'http': line.strip()} for line in file.readlines()]
+    return proxies
 
 def main():
     banner_text = """
@@ -19,12 +25,19 @@ def main():
     username = input("\033[92mEnter username: \033[0m")
     message = input("\033[92mEnter message: \033[0m")
     amount = int(input("\033[92mEnter amount: \033[0m"))
+    
+    proxy_file = "proxy.txt"
+    proxies = read_proxies_from_file(proxy_file)
 
+    proxy_pool = cycle(proxies)
+    success_count = 0
     for i in range(amount):
-        status_code = send_post_spam(username, message)
-        colored_message = f"\033[94m[{i}][{'success' if status_code == 200 else 'error'}]: Message sent to target: {username}\033[0m"
-        print(colored_message)
-        time.sleep(1) # delay for 1 econds
+        proxy = next(proxy_pool)
+        status_code = send_post_spam(username, message, proxy)
+        if status_code == 200:
+            success_count += 1
+            colored_message = f"\033[92m[{success_count}][Success]: Message sent to target: {username}\033[0m"
+            print(colored_message)
 
 if __name__ == "__main__":
     main()
